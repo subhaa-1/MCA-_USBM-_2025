715c78d5e6efe76336d90b137a4fa28e1317f87b
subhaa-1 <dassubhalaxmi2003@gmail.com>
2024-10-07 12:22:24 +0530

    template

--- frontend/javascript/class-2/notes/data.js
+++ frontend/javascript/class-2/notes/data.js
@@ -1,21 +1,21 @@
 //undefined
-//let data;
-//console.log(typeof data);
+let data;
+console.log(typeof data);
 //number
-//let num1=50;
-//let decimal_no=1.0001;
-//let pi=3.14;
+let num1=50;
+let decimal_no=1.0001;
+let pi=3.14;
 //let age=25;
-//console.log(typeof num1);
+console.log(typeof num1);
 //addition
-//let num2=100;
-//let num3=125;
-//let addition=num2+num3;
-//let substraction=num3-num2;
-//let multiplication=num2*num3;
-//console.log(addition);
-//console.log(multiplication);
-//console.log(substraction);
+let num2=100;
+let num3=125;
+let addition=num2+num3;
+let substraction=num3-num2;
+let multiplication=num2*num3;
+console.log(addition);
+console.log(multiplication);
+console.log(substraction);
 //NaN(not a number)
 let num5=0;
 let num6=0;
@@ -25,5 +25,14 @@ let ismarried=false;
 let issingle=true;
 console.log(typeof ismarried);
 console.log(typeof issingle);
-//string
+//template literal:backtik
+let name="subha";
+let age=22;
+let city="bhubaneswar"
+regd_no=2305274131;
+let college="usbm";
+let batch="mca 2nd year";
+console.log(`my name is:${name},age is:${age},city is:${city},registration no is:${regd_no},college is:${college},batch is:${batch}`)
+console.log(city.lenght);
+
 
