145d29cea9a25a2a686941b75803c3414d31e618
subhaa-1 <dassubhalaxmi2003@gmail.com>
2024-10-07 11:17:04 +0530

    addition,substraction,multiplication

---
+++ frontend/javascript/class-2/notes/data.js
@@ -0,0 +1,20 @@
+//undefined
+let data;
+console.log(typeof data);
+//number
+let num1=50;
+let decimal_no=1.0001;
+let pi=3.14;
+let age=25;
+console.log(typeof num1);
+//addition
+let num2=100;
+let num3=125;
+let addition=num2+num3;
+let substraction=num3-num2;
+let multiplication=num2*num3;
+console.log(addition);
+console.log(multiplication);
+console.log(substraction);
+
+
---
+++ frontend/javascript/class1/index.js
@@ -0,0 +1,3 @@
+console.log("hey i am subha");
+var age=25;
+let name="subha"
