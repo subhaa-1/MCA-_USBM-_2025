56f91a00dadc665b1ab3103498e787de120a5408
subhaa-1 <dassubhalaxmi2003@gmail.com>
2024-10-07 11:45:56 +0530

    addition,substraction,multiplication

--- frontend/javascript/class-2/notes/data.js
+++ frontend/javascript/class-2/notes/data.js
@@ -1,20 +1,29 @@
 //undefined
-let data;
-console.log(typeof data);
+//let data;
+//console.log(typeof data);
 //number
-let num1=50;
-let decimal_no=1.0001;
-let pi=3.14;
-let age=25;
-console.log(typeof num1);
+//let num1=50;
+//let decimal_no=1.0001;
+//let pi=3.14;
+//let age=25;
+//console.log(typeof num1);
 //addition
-let num2=100;
-let num3=125;
-let addition=num2+num3;
-let substraction=num3-num2;
-let multiplication=num2*num3;
-console.log(addition);
-console.log(multiplication);
-console.log(substraction);
-
+//let num2=100;
+//let num3=125;
+//let addition=num2+num3;
+//let substraction=num3-num2;
+//let multiplication=num2*num3;
+//console.log(addition);
+//console.log(multiplication);
+//console.log(substraction);
+//NaN(not a number)
+let num5=0;
+let num6=0;
+console.log(num5/num6);
+//boolean
+let ismarried=false;
+let issingle=true;
+console.log(typeof ismarried);
+console.log(typeof issingle);
+//string
 
