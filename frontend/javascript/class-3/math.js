b2c30d057ed654fc76bfacae046fe70a51e4ed42
subhaa-1 <dassubhalaxmi2003@gmail.com>
2024-10-08 12:00:11 +0530

    task

---
+++ frontend/javascript/class-3/math.js
@@ -0,0 +1,56 @@
+let num1=5;
+let num2=3;
+let result=num1+num2;//addition operator
+//let result=num1-num2;//substraction
+//let result=num1*num2;//multiplication
+//let result=num1/num2;//division
+
+//module operator
+let num3=29;
+let odd =num3 %2;
+console.log(odd);
+
+let square=2**2;
+let cube=2**3;
+console.log(square,cube);
+
+//equality operator
+//` ==`:value check
+//`==`value and data type check
+console.log(5==5);
+console.log(5=="5");
+console.log(5!=="5");
+
+//falsy value:
+//falsy ,0,-0,"",``,null,undefined,NaN
+//if(-100);{
+//console.log("it is true");
+//}
+
+//task
+ //given total_bill,discount start_price if you satisfy the condition print discount
+let total_bill=800
+if (total_bill>800) {
+    console.log("discount is available")
+    }
+    else{
+       console.log ("no discount")}
+
+
+
+
+//check if male and then check 22+ then he can able to marry
+let age=52;
+if(age=>60){
+    console.log("adult and senior");
+}
+else if(age>=18){
+    console.log("adult");
+
+} else{
+    console.log("a kid")
+}
+
+
+//task3
+
